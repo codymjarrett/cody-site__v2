d05e77cd9e40cb78ed166c3d4cb6d41f3d741cb8
Hero's Dad <cody.monta@gmail.com>
2019-10-25 09:04:36 -0500

    progress

--- src/components/header.js
+++ src/components/header.js
@@ -4,60 +4,52 @@ import React, { useState } from "react"
 
 const Header = () => {
   const [mobileNavOpened, setMobileNav] = useState(false)
-  // <nav className="header">
-  //   <ul className="header__group">
-  //     <li className="header__item">
-  //       <h1 className="header__title">Cody Jarrett</h1>
-  //       <div className="header__subTitles">
-  //         <span className="header__subTitles-jobTitle">Front End Engineer</span>
-  //         <span className="header__subTitles-jobTitle">UI/UX</span>
-  //       </div>
-  //     </li>
-  //     <li className="header__item">Home</li>
-  //     <li className="header__item">Blog</li>
-  //     <li className="header__item">Work</li>
-  //     <li className="header__item">Contact</li>
-  //   </ul>
-  // </nav>
 
   return (
-    <div className="container">
+    <header className="header header__container">
+      
       <button
-        className={`open-main-nav ${mobileNavOpened ? "nav-mobile--is-open" : ""}`}
+        aria-label="open/close navigation"
+        className={`open-main-nav ${
+          mobileNavOpened ? "nav-mobile--is-open" : ""
+        }`}
         onClick={() => setMobileNav(!mobileNavOpened)}
       >
-        <span
-          class="burger"
-        ></span>
+        <span class="burger"></span>
         <span class="burger-text">Menu</span>
       </button>
-      <nav
-        className={`nav-mobile ${mobileNavOpened ? "nav-mobile--is-open" : ""}`}
-      >
-        <ul className="nav-mobile__group">
-          <li className="nav-mobile__item">
+      <nav className={`header__nav ${mobileNavOpened ? "nav-mobile--is-open" : ""}`}>
+      <div className="header__title">
+        <h1 className="title">Cody Jarrett</h1>
+        <div className="header__subTitles">
+          <span className="subTitles-jobTitle">Front End Engineer</span>
+          <span className="subTitles-jobTitle">UI/UX</span>
+        </div>
+      </div>
+        <ul className="nav__group">
+          <li className="nav__item">
             <Link to="/" className="link">
               Home
             </Link>
           </li>
-          <li className="nav-mobile__item">
+          <li className="nav__item">
             <Link to="/blog" className="link">
               Blog
             </Link>
           </li>
-          <li className="nav-mobile__item">
+          <li className="nav__item">
             <Link to="/work" className="link">
               Work
             </Link>
           </li>
-          <li className="nav-mobile__item">
+          <li className="nav__item">
             <Link to="/contact" className="link">
               Contact
             </Link>
           </li>
         </ul>
       </nav>
-    </div>
+    </header>
   )
 }
 
