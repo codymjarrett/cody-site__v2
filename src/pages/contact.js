4246c149f1d2a225390595e027419686f3f50fbb
Hero's Dad <cody.monta@gmail.com>
2019-10-27 22:29:55 -0500

    contact page styling, need submission function

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,30 +11,49 @@ const ContactPage = () => (
       <h1 className="contact__title">Contact Me</h1>
       <form className="contact__form">
         <div className="contact__input">
+          <label className="contact__label" htmlFor="fullName">
+            Full Name
+          </label>
           <div className="input-decorator">
-          <label htmlFor="fullName">Full Name</label>
             <input type="text" name="fullName" id="fullName" />
           </div>
         </div>
-        <div className="contact__input">
+        {/* <div className="contact__input">
           <div className="input-decorator">
-          <label htmlFor="Email">Email</label>
+          <label className="contact__label" htmlFor="lastName">Last Name</label>
+            <input type="text" name="lastName" id="lastName" />
+          </div>
+        </div> */}
+
+        <div className="contact__input">
+          <label className="contact__label" htmlFor="phone">
+            Telephone #
+          </label>
+          <div className="input-decorator">
+            <input type="tel" name="phone" id="phone" />
+          </div>
+        </div>
+        <div className="contact__input">
+          <label className="contact__label" htmlFor="Email">
+            Email
+          </label>
+          <div className="input-decorator">
             <input type="email" name="Email" id="Email" />
           </div>
         </div>
         <div className="contact__input">
+          <label className="contact__label" htmlFor="message">
+            Message{" "}
+          </label>
           <div className="input-decorator">
-          <label htmlFor="phone">Telephone #</label>
-            <input type="text" name="phone" id="phone" />
-          </div>
-        </div>
-        <div className="contact__input">
-          <div className="input-decorator">
-          <label htmlFor="message">Message </label>
             <textarea type="text" name="message" id="message" />
           </div>
         </div>
-        <button type="submit">Send</button>
+        <div className="contact__input">
+          <button class="contact__btn" type="submit">
+            Send
+          </button>
+        </div>
       </form>
     </div>
   </Layout>
