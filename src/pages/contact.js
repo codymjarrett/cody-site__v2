7fdc0852a58efac43bb7918fce8438fd78cf48c8
Cody Jarrett <cody.jarrett@americaneagle.com>
2019-10-28 16:37:22 -0500

    form submission working

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const ContactPage = () => (
+const ContactPage = () => {
+
+  const [status, setStatus] = useState("")
+
+
+  const submitForm = ev => {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus({ status: "SUCCESS" });
+      } else {
+        setStatus({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  
+  }
+  
+  return (
+
   <Layout>
     <SEO title="Contact Page" />
     <div className="contact">
       <h1 className="contact__title">Contact Me</h1>
-      <form className="contact__form">
+      <form className="contact__form" action="https://formspree.io/xrgzybbg"
+        method="POST" onSubmit={submitForm}>
         <div className="contact__input">
           <label className="contact__label" htmlFor="fullName">
             Full Name
@@ -54,9 +81,14 @@ const ContactPage = () => (
             Send
           </button>
         </div>
+        {status === "SUCCESS" ? <p>Thanks!</p> : ""}
+        {status === "ERROR" ? <p>Opps there was an error</p> : "" }
       </form>
     </div>
   </Layout>
-)
+  )
+}
+
+
 
 export default ContactPage
