7d17e2c8bd0b86f6cc8cdc09a81caba4d44c25f0
Cody Jarrett <cody.jarrett@americaneagle.com>
2019-11-05 10:23:25 -0600

    refactored methods

--- src/components/workDesktop.js
+++ src/components/workDesktop.js
@@ -10,58 +10,72 @@ const WorkDesktop = ({ response }) => {
   }, [response])
 
  
-
   const getNewProject = id => {
     setInitialProject(id)
   }
+
+   const title = () => {
+    return response
+    .filter(p => p.node.id === initialProject)
+    .map(i => <span className="work__title">{i.node.title}</span>)
+   }
+
+   const tech = () => {
+return response
+.filter(p => p.node.id === initialProject)
+.map(i => <li className="work__item">{i.node.tech}</li>)
+   }
+
+   const image = () => {
+     return response
+     .filter(p => p.node.id === initialProject)
+     .map(i => (
+       <img
+         style={{ maxWidth: "100%", width: "30rem" }}
+         src={i.node.image.file.url}
+         alt="screen shot of project"
+       />
+     ))
+   }
+
+   const listOfTech = () => {
+     return response.map((i, idx) => {
+      return (
+        <li
+          className="work-item"
+          key={idx}
+          data-id={i.node.id}
+          onClick={id => getNewProject(i.node.id)}
+        >
+          <button>{i.node.title}</button>
+        </li>
+      )
+    })
+   }
 
   return (
     <div className="work__container">
       <div className="projects">
         <p className="projects__heading">Projects</p>
         <ul className="group work__names">
-          {response
-            ? response.map((i, idx) => {
-                return (
-                  <li
-                    className="work-item"
-                    key={idx}
-                    data-id={i.node.id}
-                    onClick={id => getNewProject(i.node.id)}
-                  >
-                    <button>{i.node.title}</button>
-                  </li>
-                )
-              })
-            : ""}
+          {response && listOfTech() }
         </ul>
       </div>
 
       <div className="work__view">
-        <span className="work__title">Dad Over-jhflow</span>
+        
+          {response && title()}
+       
         <div className="work__content">
           <div className="technology">
             <p className="technology__heading">Technologies</p>
             <ul className="group work__tech">
-              {response
-                ? response
-                    .filter(p => p.node.id === initialProject)
-                    .map(i => <li className="work__item">{i.node.tech}</li>)
-                : ""}
+              {response && tech()
+                }
             </ul>
           </div>
           <div className="work__details">
-            {response
-              ? response
-                  .filter(p => p.node.id === initialProject)
-                  .map(i => (
-                    <img
-                      style={{ maxWidth: "100%", width: "30rem" }}
-                      src={i.node.image.file.url}
-                      alt="screen shot of project"
-                    />
-                  ))
-              : ""}
+            {response && image()}
 
             <div className="buttons">
               <button type="button">Github</button>
