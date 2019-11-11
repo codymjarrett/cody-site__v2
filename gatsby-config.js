91ab56c08087719aea2fd16296783a5a89bc7b2c
Hero's Dad <cody.monta@gmail.com>
2019-11-11 12:34:54 -0600

    clean up clean up everybody everywhere

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,23 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Cody Jarrett`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Portfolio Site`,
+    author: `Cody Jarrett`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    // {
-    //   resolve: `gatsby-plugin-web-font-loader`,
-    //   options: {
-    //     google: {
-    //       families: ['Work Sans:500,600,700', 'sans-serif', 'Indie Flower', 'cursive']
-    //     }
-    //   }
-    // },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `aert94qrey0z`,
-        accessToken: `z9iyyKnhYboUhHn4ihN316Z5jhpQUZZvOGrNCX8Zv00`,
+        spaceId: process.env.GATSBY_API_SPACE_ID,
+        accessToken: process.env.GATSBY_API_ACCESS_TOKEN,
         downloadLocal: true
       },
     },
