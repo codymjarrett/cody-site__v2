b5ebc13866f617707abac1bccf091f88afbf0bf1
Hero's Dad <cody.monta@gmail.com>
2019-11-11 11:29:41 -0600

    bruh

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,9 +4,10 @@ const typography = new Typography({
   baseFontSize: "16px",
   baseLineHeight: 1.666,
   bodyFontFamily: ["Work Sans", "sans-serif"],
+  boldWeight: "700",
   googleFonts: [
     {
-      name: "work sans",
+      name: "Work Sans",
       styles: ["400", "500", "600", "700"],
     },
     {
